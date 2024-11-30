123ec574d891db2a3b5aaf5115a6bd9fc7bcfc56
Oladayo Fagbemi <oladayo.fagbemi@axelspringer.com>
2024-11-30 06:06:55 +0100

    cleanup unused blade files

---
+++ resources/js/Pages/Welcome.svelte
@@ -0,0 +1,3 @@
+<main>
+	<h1>WELCOME!</h1>
+</main>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
